ead7c68dd2108bf58b15f58a6ad9ba6cd64b9022
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-06-22 09:27:22 +0100

    add function start connect to mongodb, listen to port

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const config = require('./config/parameters')
 
 const app = express()
 
+async function start() {
+  try {
+    await mongoose.connect(config.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    app.listen(config.PORT, function(){
+      console.log('Mendengar di PORT nombor 3000')
+    })
+  } catch(e) {
+    console.log(e)
+  }
+}
+
 app.get('/', function (req, res) {
     res.send('Contact API')
   })
-
-app.listen(config.PORT, function(){
-    console.log('Mendengar di PORT nombor 3000')
-})
